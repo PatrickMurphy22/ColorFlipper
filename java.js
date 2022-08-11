d1b725c228449480cd11d5b6498ec3ee0dea693d
Patrick Murphy <murphy866530@gmail.com>
2022-08-11 12:41:52 +0100

    Basic color flipper created. More HTML to be added along with more Javascript

--- java.js
+++ java.js
@@ -1,1 +1,15 @@
+// Color array
+const colors = ["red","blue","green","orange","yellow"];
 
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function (){ 
+    //pick random number from Color Array
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber]
+})
+
+function getRandomNumber (){
+    return Math.floor (Math.random() *colors.length);
+}
