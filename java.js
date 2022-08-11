d224826d3a0c3a58550b2a2fea8d37af16581191
Patrick Murphy <murphy866530@gmail.com>
2022-08-11 15:19:50 +0100

    added more colors and text to state which color is being displayed

--- java.js
+++ java.js
@@ -1,15 +1,19 @@
 // Color array
-const colors = ["red","blue","green","orange","yellow"];
+const colors = ["red","blue","green","orange","yellow", "black", "white",
+ "silver", "purple","violet","pink","aqua",];
 
 const btn = document.getElementById("btn");
+const color = document.querySelector(".color")
 
 btn.addEventListener("click", function (){ 
-    //pick random number from Color Array
+    // assing getRandomNumber function to randomNumber
     const randomNumber = getRandomNumber();
-
+    //change color of background using the fucntion with the array
     document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber];
 })
 
+//Function to get random color 
 function getRandomNumber (){
-    return Math.floor (Math.random() *colors.length);
+    return Math.floor (Math.random() * colors.length);
 }
